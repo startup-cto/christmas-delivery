292d6e92c0543e3be0f036e1466de45a50f8391c
Daniel Bartholomae <daniel@bartholomae.name>
2020-12-07 22:11:52 +0100

    feat: pass time automatically

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { useDispatch, useSelector } from "react-redux";
 import { State } from "./components/Display/State";
 import { Display } from "./components/Display/Display";
-import React from "react";
+import React, { useEffect } from "react";
 import { actions } from "./world/slice";
 
 export function App() {
   const state = useSelector((state: State) => state);
   const dispatch = useDispatch();
+  useEffect(() => {
+    const timer = setInterval(() => dispatch(actions.waitTicks(1)), 1000);
+    return () => clearInterval(timer);
+  }, [dispatch]);
+
   return (
     <>
       <Display state={state} />
