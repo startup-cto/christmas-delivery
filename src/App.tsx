a5950745023ef36b520979eac4f833c0bf64d9f2
Daniel Bartholomae <daniel@bartholomae.name>
2020-12-07 20:31:47 +0100

    refactor: save state in redux

--- src/App.tsx
+++ src/App.tsx
@@ -2,36 +2,43 @@ import React from "react";
 import { Display } from "./Display";
 import { Pixel } from "./models/Pixel";
 import { PixelPerSecond } from "./models/PixelPerSecond";
+import { configureStore } from "@reduxjs/toolkit";
 
-function App() {
-  const state = {
-    houses: [
-      {
-        id: "1",
-        position: {
-          x: 100 as Pixel,
-          y: 100 as Pixel,
-        },
-      },
-    ],
-    sleighs: [
-      {
-        id: "1",
-        maxSpeed: 5 as PixelPerSecond,
-        position: {
-          x: 400 as Pixel,
-          y: 300 as Pixel,
-        },
-        commands: [],
-      },
-    ],
-    world: {
-      size: {
-        width: 800 as Pixel,
-        height: 600 as Pixel,
+const defaultState = {
+  houses: [
+    {
+      id: "1",
+      position: {
+        x: 100 as Pixel,
+        y: 100 as Pixel,
       },
     },
-  };
+  ],
+  sleighs: [
+    {
+      id: "1",
+      maxSpeed: 5 as PixelPerSecond,
+      position: {
+        x: 400 as Pixel,
+        y: 300 as Pixel,
+      },
+      commands: [],
+    },
+  ],
+  world: {
+    size: {
+      width: 800 as Pixel,
+      height: 600 as Pixel,
+    },
+  },
+};
+
+const store = configureStore({
+  reducer: (state = defaultState, action) => state,
+});
+
+function App() {
+  const state = store.getState();
   return <Display state={state} />;
 }
 
