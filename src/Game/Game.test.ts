ea4811754b7760d03eaf9f480e3bd309546c89dc
Daniel Bartholomae <daniel@bartholomae.name>
2020-12-08 21:16:43 +0100

    feat: game has access to houses

--- src/Game/Game.test.ts
+++ src/Game/Game.test.ts
@@ -6,26 +6,44 @@ import { Command } from "../sleighs/Command";
 import { State } from "../components/Display/State";
 
 describe("Game", () => {
-  it("returns the currently available sleighs", () => {
-    const initialState: State = {
-      houses: [],
-      world: { size: { width: 100 as Pixel, height: 100 as Pixel } },
-      sleighs: [
-        {
-          id: "1",
-          maxSpeed: 5 as PixelPerTick,
-          position: {
-            x: 0 as Pixel,
-            y: 0 as Pixel,
-          },
-          commands: [] as Command[],
+  const initialState: State = {
+    houses: [
+      {
+        id: "1",
+        position: {
+          x: 100 as Pixel,
+          y: 100 as Pixel,
         },
-      ],
-    };
-    const store = createStore<State, AnyAction, unknown, unknown>(
-      () => initialState
-    );
-    const game = new Game(store);
-    expect(game.sleighs).toEqual(initialState.sleighs);
+      },
+    ],
+    world: { size: { width: 100 as Pixel, height: 100 as Pixel } },
+    sleighs: [
+      {
+        id: "1",
+        maxSpeed: 5 as PixelPerTick,
+        position: {
+          x: 0 as Pixel,
+          y: 0 as Pixel,
+        },
+        commands: [] as Command[],
+      },
+    ],
+  };
+  const store = createStore<State, AnyAction, unknown, unknown>(
+    () => initialState
+  );
+
+  describe("#sleighs", () => {
+    it("returns the currently available sleighs", () => {
+      const game = new Game(store);
+      expect(game.sleighs).toEqual(initialState.sleighs);
+    });
+  });
+
+  describe("#houses", () => {
+    it("returns the currently available houses", () => {
+      const game = new Game(store);
+      expect(game.houses).toEqual(initialState.houses);
+    });
   });
 });
--- src/Game/Game.ts
+++ src/Game/Game.ts
@@ -1,8 +1,13 @@
 import { Action, Store } from "@reduxjs/toolkit";
 import { Sleigh } from "../sleighs/Sleigh";
 import { State } from "../components/Display/State";
+import { House } from "../houses/House";
 
 export class Game {
+  get houses(): House[] {
+    return this.store.getState().houses;
+  }
+
   get sleighs(): Sleigh[] {
     return this.store.getState().sleighs;
   }
