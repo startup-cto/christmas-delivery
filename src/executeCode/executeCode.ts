b896f1c9e456503d91f47250bb5594d96774fe88
Daniel Bartholomae <daniel@bartholomae.name>
2020-12-08 22:01:03 +0100

    fix: ignore warning for new function

--- src/executeCode/executeCode.ts
+++ src/executeCode/executeCode.ts
@@ -1,6 +1,7 @@
 import { Game } from "../Game/Game";
 
 export function executeCode(code: string, game: Game) {
+  // eslint-disable-next-line no-new-func
   const execute = new Function("game", code);
   execute(game);
 }
