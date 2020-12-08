1e89524b4643044c8fdef687a0d49d2345cbc86c
Daniel Bartholomae <daniel@bartholomae.name>
2020-12-08 20:14:57 +0100

    feat: add button to tell Santa to move to the house

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,35 @@ import { useDispatch, useSelector } from "react-redux";
 import { State } from "./components/Display/State";
 import { Display } from "./components/Display/Display";
 import React, { useEffect } from "react";
-import { actions } from "./world/slice";
+import { actions as worldActions } from "./world/slice";
+import { actions as sleighActions } from "./sleighs/slice";
+import { Pixel } from "./models/Pixel";
 
 export function App() {
   const state = useSelector((state: State) => state);
   const dispatch = useDispatch();
   useEffect(() => {
-    const timer = setInterval(() => dispatch(actions.waitTicks(1)), 1000);
+    const timer = setInterval(() => dispatch(worldActions.waitTicks(1)), 100);
     return () => clearInterval(timer);
   }, [dispatch]);
 
   return (
     <>
       <Display state={state} />
-      <button onClick={() => dispatch(actions.waitTicks(1))}>
-        Wait 1 tick
+      <button
+        onClick={() =>
+          dispatch(
+            sleighActions.moveSleigh({
+              sleighId: "1",
+              targetPosition: {
+                x: 100 as Pixel,
+                y: 100 as Pixel,
+              },
+            })
+          )
+        }
+      >
+        Tell santa to move to the house!
       </button>
     </>
   );
