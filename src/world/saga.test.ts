96df3493178dc870caa51a19bf4032bdce6f45ca
Daniel Bartholomae <daniel@bartholomae.name>
2020-12-21 13:17:51 +0100

    test: runTicks saga dispatches one more waitTicks action than delay effects resolved

--- src/world/saga.test.ts
+++ src/world/saga.test.ts
@@ -5,13 +5,15 @@ import { actions as levelActions } from "../currentLevel/slice";
 import { MockSleigh } from "../sleighs/MockSleigh";
 import { MockHouse } from "../houses/MockHouse";
 import { Pixel } from "../models/Pixel";
+import { put } from "redux-saga/effects";
+import { equals } from "ramda";
 
 describe("worldSaga", () => {
   describe("runTicks", () => {
     describe("if the game is running", () => {
       const isRunning = true;
 
-      it("dispatches waitTicks", async () => {
+      it("dispatches only waitTicks actions", async () => {
         const world = {
           fps: 50,
           isRunning,
@@ -19,15 +21,49 @@ describe("worldSaga", () => {
           ticks: 0,
           ticksPerFrame: 1,
         };
-        await expectSaga(worldSaga)
-          .put(actions.waitTicks(world.ticksPerFrame))
+        const result = await expectSaga(worldSaga)
           .withState({
             sleighs: [],
             houses: [],
             world,
           })
           .dispatch(actions.runGame(world))
+          .delay(1000 / world.fps)
           .silentRun();
+        expect(result.effects.put).toSatisfyAll(
+          equals(put(actions.waitTicks(world.ticksPerFrame)))
+        );
+      });
+
+      it("dispatches one more waitTicks action than delay effects resolved", async () => {
+        let resolvedDelayEffects = 0;
+        const world = {
+          fps: 50,
+          isRunning,
+          size: { width: 100 as Pixel, height: 100 as Pixel },
+          ticks: 0,
+          ticksPerFrame: 1,
+        };
+        const result = await expectSaga(worldSaga)
+          .withState({
+            sleighs: [],
+            houses: [],
+            world,
+          })
+          .provide({
+            call({ fn }, next) {
+              if (fn.name === "delayP") {
+                resolvedDelayEffects++;
+                if (resolvedDelayEffects >= 3) {
+                  throw new Error("Stop execution");
+                }
+              }
+              next();
+            },
+          })
+          .dispatch(actions.runGame(world))
+          .silentRun();
+        expect(result.effects.put.length).toBe(resolvedDelayEffects + 1);
       });
     });
   });
