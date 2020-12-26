94e11fced8546ca65ea19fe913f3275f48ed978e
Daniel Bartholomae <daniel@bartholomae.name>
2020-12-26 23:28:04 +0100

    feat: add TreeSprite

---
+++ src/trees/TreeSprite/TreeSprite.tsx
@@ -0,0 +1,30 @@
+import { Sprite } from "../../components/Sprite/Sprite";
+import treeSpriteSheet from "./tree.png";
+import { Pixel } from "../../models/Pixel";
+import React from "react";
+import { Position } from "../../models/Position";
+
+const treeSpriteSheetMetaData = {
+  source: treeSpriteSheet,
+  size: {
+    width: 64 as Pixel,
+    height: 96 as Pixel,
+  },
+  frames: 1,
+  states: ["idle"] as const,
+};
+
+export interface Props {
+  position: Position;
+}
+
+export function TreeSprite({ position }: Props) {
+  return (
+    <Sprite
+      position={position}
+      spriteSheet={treeSpriteSheetMetaData}
+      scale={1}
+      state="idle"
+    />
+  );
+}
