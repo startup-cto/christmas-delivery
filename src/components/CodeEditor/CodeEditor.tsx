6d79909df2ee0e5f7c6fbc5c26f3da8de769aa18
Daniel Bartholomae <daniel@bartholomae.name>
2020-12-27 00:34:24 +0100

    fix: make code editor label nicer

--- src/components/CodeEditor/CodeEditor.tsx
+++ src/components/CodeEditor/CodeEditor.tsx
@@ -4,6 +4,12 @@ import { codeInputLabel, runCodeButtonLabel } from "../../locale/en/main.json";
 import AceEditor from "react-ace";
 import "ace-builds/src-noconflict/mode-javascript";
 import "ace-builds/src-noconflict/theme-monokai";
+import styled from "styled-components";
+
+const Heading = styled.h2`
+  font-family: Mountains of Christmas;
+  margin: 0;
+`;
 
 interface Props {
   code: string;
@@ -20,17 +26,20 @@ export const CodeEditor: FunctionComponent<Props> = ({
 }) => (
   <form>
     <label>
-      {codeInputLabel}
+      <Heading>{codeInputLabel}</Heading>
       <AceEditor
         readOnly={disabled}
         value={code}
         mode="javascript"
         theme="monokai"
         onChange={onCodeChange}
-        height="5rem"
+        height="17rem"
         width="100%"
         setOptions={{
           useWorker: false,
+        }}
+        style={{
+          margin: "1rem 0",
         }}
       />
     </label>
